871b474ddd7dbee1d639b09df713e4af46c83d27
felipessgg <fsepulvedagalvez@gmail.com>
2025-08-06 21:59:10 +1000

    I have created the Student class, also print name and ID

--- StudentMarkManager.java
+++ StudentMarkManager.java
@@ -11,7 +11,7 @@ public class StudentMarkManager {
     public static void main(String[] args) {
         
         String fileName = "students.txt";
-        List<String> lines = new ArrayList<>();
+        List<Student> students = new ArrayList<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
@@ -19,11 +19,37 @@ public class StudentMarkManager {
                 if (line.trim().isEmpty() || line.startsWith("Unit") || line.startsWith("Last Name") || line.startsWith("#")) {
                     continue;
                 }
-                lines.add(line);
+                
+                String[] parts = line.split(",");
+                if(parts.length < 3) continue;
+                
+                String fullName = parts[1].trim() + " " + parts[0].trim();
+                String id = parts[2].trim();
+                
+                Student student = new Student(fullName, id);
+                students.add(student);
             }
-            System.out.println("File read successfully wujuuuu. Total students: " + lines.size());
+            System.out.println("Students loaded:\n");
+            for (Student s : students){
+                s.printInfo();
+            }
+            
         } catch (IOException e) {
             System.out.println("Error reading the file: " + e.getMessage());
         }
+    }
+}
+
+class Student {
+    private String name;
+    private String id;
+    
+    public Student (String name, String id){
+        this.name = name;
+        this.id = id;
+    }
+    
+    public void printInfo(){
+        System.out.println(name + " (" + id + ") ");
     }
 }
